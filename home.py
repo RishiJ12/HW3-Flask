16980499f03f5f92f7451a9fc89d6514564a6c9b
RishiJ12 <rishijagtap12@gmail.com>
2022-03-23 22:25:38 -0700

    home.py

---
+++ home.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import escape
+
+myapp_obj = Flask(__name__)
+
+@myapp_obj.route("/")
+def home():
+    name = "Rishi"
+    city_names = ["Paris", "London", "Rome", "Tahiti"]
+    return '''
+    <html>
+    <head>
+        <h1><strong>  Welcome ''' + name + '''</strong> </h1>
+        <a href="www.google.com"> not google </a>
+    </head>
+    <ul style=“list-style-type:square”>
+
+    <li>''' + city_names[0] + '''</li>
+
+    <li>''' + city_names[1] + '''</li>
+
+    <li>''' + city_names[2] + '''</li>
+
+    <li>''' + city_names[3] + '''</li>
+
+    </ul>
+    </html>'''
+
+myapp_obj.run()
+
