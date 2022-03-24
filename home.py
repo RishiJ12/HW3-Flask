a28fc39198162ca5b41ae9bec5c9d0fb5e5907a4
RishiJ12 <rishijagtap12@gmail.com>
2022-03-23 22:48:58 -0700

    commit

--- home.py
+++ home.py
@@ -1,29 +1,13 @@
+
 from flask import Flask
+from flask import render_template
 
 myapp_obj = Flask(__name__)
 
 @myapp_obj.route("/")
 def home():
-    name = "Rishi"
-    city_names = ["Paris", "London", "Rome", "Tahiti"]
-    return '''
-    <html>
-    <head>
-        <h1><strong>  Welcome ''' + name + '''</strong> </h1>
-        <a href="www.google.com"> not google </a>
-    </head>
-    <ul style=“list-style-type:square”>
-
-    <li>''' + city_names[0] + '''</li>
-
-    <li>''' + city_names[1] + '''</li>
-
-    <li>''' + city_names[2] + '''</li>
-
-    <li>''' + city_names[3] + '''</li>
-
-    </ul>
-    </html>'''
+    name
+    city_names = []
+    return render_template("home.html", len = len(city_names), city_names = city_names)
 
 # myapp_obj.run()
-
