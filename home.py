b008ffc195235c74c4ef3ee1dbd072b64f54484d
RishiJ12 <rishijagtap12@gmail.com>
2022-03-28 20:47:25 -0700

    home.py

--- home.py
+++ home.py
@@ -4,6 +4,7 @@ from flask import render_template
 
 myapp_obj = Flask(__name__)
 name = ""
+city_names = []
 @myapp_obj.route("/")
 def home():
     return render_template("home.html", city_names = city_names, name = name)
