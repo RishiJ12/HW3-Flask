cd6f5be4dc13aeffd510ddb9c9005c18e7376eb1
RishiJ12 <rishijagtap12@gmail.com>
2022-03-28 21:14:09 -0700

    home.py

--- home.py
+++ home.py
@@ -1,13 +1,34 @@
 
 from flask import Flask
-from flask import render_template
 
 myapp_obj = Flask(__name__)
 name = ""
 city_names = []
 @myapp_obj.route("/")
 def home():
-    return render_template("home.html", city_names = city_names, name = name)
+    return '''
+    <html>
+    <head>
+         <h1><strong>  Welcome ''' + name + '''</strong> </h1>
+        <a href="www.google.com"> not google </a>
+    </head>
+    <ul style = "line-height:0;">
+            <li>''' + city_names[0] + ''' </li>
+    </ul>
+    <ul style = "line-height:0;">
+            <li>''' + city_names[1] + ''' </li>
+    </ul>
+    <ul style = "line-height:0;">
+            <li>''' + city_names[2] + ''' </li>
+    </ul>
+    <ul style = "line-height:0;">
+            <li>''' + city_names[3] + ''' </li>
+    </ul>
+
+
+
+</html>'''
+
 
 
 
