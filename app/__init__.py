b90632f8b81fe92a73478ae1aad16547df61e161
RishiJ12 <rishijagtap12@gmail.com>
2022-05-17 19:36:49 -0700

    Everything works

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
  
 myapp_obj = Flask(__name__)
- 
+myapp_obj.secret_key = 'QWERTFDS13124351' 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
 from app import myapp_obj
-from flask import render_template
+from flask import render_template, request, flash
 
 name = "Rishi"
 city_names = ["Paris", "London", "Rome", "Tahiti"]
 @myapp_obj.route("/")
 def home():
     return render_template("home.html", city_names = city_names, name = name)
+
+@myapp_obj.route('/', methods=['GET', 'POST'])
+def show_text():
+    text = request.form['text']
+    if request.method == 'POST':
+        if text != "":
+            flash(text)
+    return render_template("home.html", city_names = city_names, name = name)
+
+
+
+
+
